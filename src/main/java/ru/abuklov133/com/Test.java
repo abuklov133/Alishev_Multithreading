2ccab0479d94a4e0f80c07655ebc920e4c70823e
abuklov133 <abuklov133@gmail.com>
2024-02-26 14:53:44 +0300

    ExecutorService

--- src/main/java/ru/abuklov133/com/Test.java
+++ src/main/java/ru/abuklov133/com/Test.java
@@ -5,6 +5,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 public class Test {
+
     public static void main(String[] args) throws InterruptedException {
         ExecutorService executorService = Executors.newFixedThreadPool(2);
         for (int i = 0; i < 5; i++) {
@@ -17,6 +18,7 @@ public class Test {
 }
 
     class Work implements Runnable {
+
         private int id;
 
         public Work(int id) {
